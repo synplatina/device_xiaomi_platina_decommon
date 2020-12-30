2df1a172fd4dcf486114d346f4861555687d908f
SynPlatina <rizalnoerdin13@gmail.com>
2020-12-30 05:51:31 +0000

    Bringup Resurrection Remix

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -22,9 +22,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_platina.mk
+    $(LOCAL_DIR)/rr_platina.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_platina-eng \
-    aosp_platina-userdebug \
-    aosp_platina-user
+    rr_platina-eng \
+    rr_platina-userdebug \
+    rr_platina-user
