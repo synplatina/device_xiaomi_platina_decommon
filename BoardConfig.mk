7def8c5714756d97b02f23ee833ac71b4c80e1a6
synplatina <rizalnoerdin13@gmail.com>
2021-03-12 07:20:05 +0000

    platina: Switch kernel to Morph

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_VERSION := 4.4
 TARGET_KERNEL_SOURCE := kernel/xiaomi/platina
 TARGET_KERNEL_CONFIG := platina_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 KERNEL_TOOLCHAIN := $(PWD)/compiler/bin
 
 # Enable stats logging in LMKD
