24af4737968d0c6b906eb302c531df9c49130d6a
synplatina <rizalnoerdin13@gmail.com>
2021-07-29 03:30:37 +0000

    platina: Compile using clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,8 +154,10 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_VERSION := 4.4
 TARGET_KERNEL_SOURCE := kernel/xiaomi/platina
 TARGET_KERNEL_CONFIG := platina_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-KERNEL_TOOLCHAIN := $(PWD)/compiler/bin
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
